1fbbf9c5f131998c0e343c1ad7852c57befacff4
Colin Adler <colin@coder.com>
2020-11-18 10:49:19 -0600

    Use schema qualifier on foreign key tables (#10)

--- queue/pgq/sql.go
+++ queue/pgq/sql.go
@@ -2,65 +2,65 @@ package pgq
 
 const bootstrapDB = `
 CREATE TABLE IF NOT EXISTS {schemaName}.jobs (
-id text NOT NULL PRIMARY KEY,
-type text NOT NULL,
-queue_group text DEFAULT ''::text NOT NULL,
-version integer NOT NULL,
-priority integer  NOT NULL
+	id text NOT NULL PRIMARY KEY,
+	type text NOT NULL,
+	queue_group text DEFAULT ''::text NOT NULL,
+	version integer NOT NULL,
+	priority integer  NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS {schemaName}.job_body (
-id text NOT NULL PRIMARY KEY,
-job jsonb NOT NULL,
-FOREIGN KEY (id) REFERENCES jobs(id) ON DELETE CASCADE
+	id text NOT NULL PRIMARY KEY,
+	job jsonb NOT NULL,
+	FOREIGN KEY (id) REFERENCES {schemaName}.jobs(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS {schemaName}.job_scopes (
-id text NOT NULL,
-scope text UNIQUE NOT NULL,
-FOREIGN KEY (id) REFERENCES jobs(id) ON DELETE CASCADE
+	id text NOT NULL,
+	scope text UNIQUE NOT NULL,
+	FOREIGN KEY (id) REFERENCES {schemaName}.jobs(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS {schemaName}.job_status (
-id text NOT NULL PRIMARY KEY,
-owner text NOT NULL,
-completed boolean NOT NULL,
-in_progress boolean NOT NULL,
-mod_ts timestamptz NOT NULL,
-mod_count integer NOT NULL,
-err_count integer NOT NULL,
-FOREIGN KEY (id) REFERENCES jobs(id) ON DELETE CASCADE
+	id text NOT NULL PRIMARY KEY,
+	owner text NOT NULL,
+	completed boolean NOT NULL,
+	in_progress boolean NOT NULL,
+	mod_ts timestamptz NOT NULL,
+	mod_count integer NOT NULL,
+	err_count integer NOT NULL,
+	FOREIGN KEY (id) REFERENCES {schemaName}.jobs(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS {schemaName}.job_errors (
-id text NOT NULL,
-error text NOT NULL,
-FOREIGN KEY (id) REFERENCES jobs(id) ON DELETE CASCADE
+	id text NOT NULL,
+	error text NOT NULL,
+	FOREIGN KEY (id) REFERENCES {schemaName}.jobs(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS {schemaName}.job_time (
-id text NOT NULL PRIMARY KEY,
-created timestamptz NOT NULL,
-started timestamptz NOT NULL,
-ended timestamptz NOT NULL,
-wait_until timestamptz NOT NULL,
-dispatch_by timestamptz NOT NULL,
-max_time integer NOT NULL,
-FOREIGN KEY (id) REFERENCES jobs(id) ON DELETE CASCADE
+	id text NOT NULL PRIMARY KEY,
+	created timestamptz NOT NULL,
+	started timestamptz NOT NULL,
+	ended timestamptz NOT NULL,
+	wait_until timestamptz NOT NULL,
+	dispatch_by timestamptz NOT NULL,
+	max_time integer NOT NULL,
+	FOREIGN KEY (id) REFERENCES {schemaName}.jobs(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS {schemaName}.dependency (
-id text NOT NULL PRIMARY KEY,
-dep_type text NOT NULL,
-dep_version integer NOT NULL,
-dependency jsonb NOT NULL,
-FOREIGN KEY (id) REFERENCES jobs(id) ON DELETE CASCADE
+	id text NOT NULL PRIMARY KEY,
+	dep_type text NOT NULL,
+	dep_version integer NOT NULL,
+	dependency jsonb NOT NULL,
+	FOREIGN KEY (id) REFERENCES {schemaName}.jobs(id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS {schemaName}.dependency_edges (
-id text NOT NULL NOT NULL,
-edge text NOT NULL,
-FOREIGN KEY (id) REFERENCES jobs(id) ON DELETE CASCADE
+	id text NOT NULL NOT NULL,
+	edge text NOT NULL,
+	FOREIGN KEY (id) REFERENCES {schemaName}.jobs(id) ON DELETE CASCADE
 );
 
 CREATE INDEX IF NOT EXISTS jobs_queue_group ON {schemaName}.jobs (queue_group);
